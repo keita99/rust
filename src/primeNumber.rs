0c88ed0f3ee109b0c9122bb3d1449e6158b9e333
Keita.Nakagami <keitanakagami11@aim.com>
2018-04-29 12:36:22 +0900

    prime number 書いてみた

--- src/primeNumber.rs
+++ src/primeNumber.rs
@@ -3,9 +3,15 @@ fn hello_world() {
 }
 
 fn main() {
-    hello_world();
+	for x in 1..10 {
+		println!( x );
+	}
 }
 
-fn prime_numberI() {
-    
+fn prime_numberI( z: i32) {
+	for x in 0..z {
+		for y in 0..z {
+			if z >= x * y { break; }
+		}
+	}    
 }
